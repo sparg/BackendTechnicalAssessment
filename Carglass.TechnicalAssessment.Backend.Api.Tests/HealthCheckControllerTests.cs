d1cd3a63bf7df9c250f3c7b6f8fd2f299f03ee2c
sparg <ivangv90@gmail.com>
2024-12-01 02:53:38 +0100

    Added HealthCheck controller with testing

---
+++ Carglass.TechnicalAssessment.Backend.Api.Tests/HealthCheckControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Carglass.TechnicalAssessment.Backend.Api.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carglass.TechnicalAssessment.Backend.Api.Tests;
+
+public class HealthCheckControllerTests
+{
+    [Fact]
+    public void KeepAlive_ReturnsOkResult()
+    {
+        // Arrange
+        var controller = new HealthCheckController();
+
+        // Act
+        var result = controller.KeepAlive();
+
+        // Assert
+        result.Should().BeOfType<OkResult>();
+    }
+
+    [Fact]
+    public void KeepAlive_ReturnsStatusCode200()
+    {
+        // Arrange
+        var controller = new HealthCheckController();
+
+        // Act
+        var result = controller.KeepAlive() as OkResult;
+
+        // Assert
+        result.Should().NotBeNull();
+        result?.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public void KeepAlive_HandlesExceptionGracefully()
+    {
+        // Arrange
+        var controller = new HealthCheckController();
+        // Simulate an exception scenario if applicable
+
+        // Act
+        Action act = () => controller.KeepAlive();
+
+        // Assert
+        act.Should().NotThrow();
+    }
+}
---
+++ Carglass.TechnicalAssessment.Backend.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Carglass.TechnicalAssessment.Backend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet("KeepAlive")]
+        public IActionResult KeepAlive()
+        {
+            return Ok();
+        }
+    }
+}
